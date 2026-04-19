ec6222f7ed92635b4a7e9e89b67289f57a8f1081
pkinge01 <51260439+pkinge01@users.noreply.github.com>
2026-04-19 09:48:17 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -19,7 +19,7 @@ navClose.addEventListener("click", () => {
   }
 });
 
-// Fixed Nav
+// Fixed Navbar
 const navBar = document.querySelector(".nav");
 const navHeight = navBar.getBoundingClientRect().height;
 window.addEventListener("scroll", () => {
